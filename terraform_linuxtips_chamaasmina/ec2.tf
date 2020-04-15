270321a9f8544c981f3e27bd4a0db250a96ca916
Barbara Becker <bbeckerdarosa@gmail.com>
2020-04-15 12:02:27 -0300

    add poc terraform using for and for_each loop

--- terraform_linuxtips_chamaasmina/ec2.tf
+++ terraform_linuxtips_chamaasmina/ec2.tf
@@ -13,7 +13,8 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  for_each      = toset(var.instance_type)
+  instance_type = each.value
   tags = {
     Name = "HelloWorld-Terraform"
   }
--- terraform_linuxtips_chamaasmina/output.tf
+++ terraform_linuxtips_chamaasmina/output.tf
@@ -1,3 +1,8 @@
 output "ip_address" {
-  value = "${aws_instance.web[*].public_ip}"
+  // value = "${aws_instance.web[*].public_ip}"
+  // o sinal => mostra que existe uma relaçao entre o instance id e instance private ip
+  value = {
+    for instance in aws_instance.web:
+    instance.id => instance.private_ip
+  }
 }
